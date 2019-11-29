ec6170f3b2e8226261e8462966f34939a09a80bb
yusyaboykowa <55316927+yusyaboykowa@users.noreply.github.com>
2019-11-29 20:40:18 +0200

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,8 +1,15 @@
-alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghjklmnopqrstuvwxyzabcdefghjklmnopqrstuvwxyz01234567890123456789АБВГДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯабвгдеєжзиіїклмнопрстуфхцчшщьюя"
-step =1
-text = input("Please write your text ").strip()
-res = ''
-for c in text:
-    res += alphabet[(alphabet.index(c) + step)%len(alphabet)]
-print("Result: " + res + "")
+def sort(a):
+    wordlist = a.split()
+    wordlist.sort()
+    return wordlist
+
+while True:
+    a = str(input ("enter a word: "))
+    choice = int(input("1 - count char\n2 - Sort the sentence\n"))
+    if(choice == 1):
+        symbol = str(input("enter a symbol: "))
+        print(a.count(symbol))
+    elif(choice == 2):
+        for word in sort(a):
+            print(word)
 
